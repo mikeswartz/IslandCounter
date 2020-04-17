84cd9f825ee8b25b7a34274694d75d0920d76144
Mike Swartz <mikeswartz@Mikes-MBP.T-mobile.com>
2020-04-17 07:37:41 -0700

    bind dfs in constructor

--- islandcount.js
+++ islandcount.js
@@ -26,17 +26,19 @@ class IslandCounter {
         this.grid = grid;
         this.i = 0;
         this.j = 0;
-        this.dfs = (grid, i, j) => {
-            if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
-                return 0;
-            }
-            grid[i][j] = 0;
-            this.dfs(grid, i + 1, j);
-            this.dfs(grid, i - 1, j);
-            this.dfs(grid, i, j + 1);
-            this.dfs(grid, i, j - 1);
-            return 1;
+        this.dfs = this.dfs.bind(this);
+    }
+
+    dfs = (grid, i, j) => {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
+            return 0;
         }
+        grid[i][j] = 0;
+        this.dfs(grid, i + 1, j);
+        this.dfs(grid, i - 1, j);
+        this.dfs(grid, i, j + 1);
+        this.dfs(grid, i, j - 1);
+        return 1;
     }
 
     get islandCount() {
